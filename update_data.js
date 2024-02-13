17d648c2978ffdd48e9940ea5b800ec5b74e4585
Abdelhak Marouane <am0089@uah.com>
2024-02-13 13:25:58 -0600

    Auto-update EMIT

--- update_data.js
+++ update_data.js
@@ -1,10 +1,42 @@
 // import fs
 const fs = require("fs");
 
+const COMBINED_METADATA_ENDPOINT = "https://earth.jpl.nasa.gov/emit-mmgis-lb/Missions/EMIT/Layers/coverage/combined_plume_metadata.json";
+const STAC_ENDPOINT = "https://ghg.center/api/stac/collections/emit-ch4plume-v1/items?limit=500";
+
+const get_methane_geojson = async () => {
+var features = [];
+var there_is_more_data = true;
+var endpoint = STAC_ENDPOINT;
+var methane_stac_geojson;
+while (there_is_more_data) {
+
+ methane_stac_geojson= await (
+ await fetch(endpoint)
+ ).json();
+ features = features.concat(methane_stac_geojson.features);
+let links = methane_stac_geojson.links;
+
+for (const link of links) {
+there_is_more_data = false;
+ if (link.rel === "next") {
+ there_is_more_data = true;
+ endpoint = link.href;
+ break;
+ }
+
+}
+}
+methane_stac_geojson.features = features;
+return methane_stac_geojson
+
+}
+
+
 async function main() {
 // fetch data from the stac endpoint
 const methane_stac_data = await (
-  await fetch("https://earth.jpl.nasa.gov/emit-mmgis-lb/Missions/EMIT/Layers/coverage/combined_plume_metadata.json")
+  await fetch(COMBINED_METADATA_ENDPOINT)
   ).json();
 // Write the data to a file
 fs.writeFileSync(
@@ -14,9 +46,7 @@ fs.writeFileSync(
 
 
  // fetch data from https://ghg.center/api/stac/collections/emit-ch4plume-v1/items?limit=1000
- const methane_stac_geojson= await (
- await fetch("https://ghg.center/api/stac/collections/emit-ch4plume-v1/items?limit=1000")
- ).json();
+ const methane_stac_geojson= await get_methane_geojson();
 
  // Write the data to a file
  fs.writeFileSync(
@@ -26,4 +56,6 @@ fs.writeFileSync(
 
 }
 
-main()
+ main()
+
+
